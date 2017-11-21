29b311ad7a695a2f9c87da09891ec5f27b6c7c90
Jason Salmanowitz <jsalmanowitz@ucvts.org>
2017-11-21 14:47:04 +0000

    Completed exercise1

--- exercise1.py
+++ exercise1.py
@@ -12,7 +12,7 @@ print(f"One of my cats is named {cat2_name}")
 #  I don't actually need to create a variable just to store these values.
 print(f"{cat2_name} has {cat2['fur']} {cat2['color']} fur.")
 
-# Replace the empty string with the necessary code to assign cat's name (from the dictionary) to the variable 
+# Replace the empty string with the necessary code to assign cat's name (from the dictionary) to the variable
 cat_name = cat['name']
 
 # Fill in the brackets in the formatted string so the string is correct. Make sure you use the values from the dictionary.
@@ -20,10 +20,10 @@ cat_name = cat['name']
 # I hope you like cats.
 # If you don't want to hear about cats, write me a better story and I'll use it for the next example
 
-the_story_of_fred = f"""{"__INSERT NAME HERE__"} is a {"__INSERT PERSONALITY HERE__"} {"__INSERT SPECIES HERE__"}.
-He was found as a stray {"__INSERT AGE HERE__"} years ago in a pothole on a busy street in Philadelphia.
-{"__INSERT NAME HERE__"} ate a lot, and soon grew to be {"__INSERT SIZE HERE__"}.
-He has {"__INSERT FUR LENGTH HERE__"} {"__INSERT COLOR HERE__"} fur and loves {"__INSERT INTERESTS HERE__"}
+the_story_of_fred = f"""{cat_name} is a {cat["personality"]} {cat["species"]}.
+He was found as a stray {cat["age"]} years ago in a pothole on a busy street in Philadelphia.
+{cat_name} ate a lot, and soon grew to be {cat["size"]}.
+He has {cat["fur"]} {cat["color"]} fur and loves {cat["interests"]}
 """
 
 # If you've done this exercise correctly, you'll hear about one of my cats
